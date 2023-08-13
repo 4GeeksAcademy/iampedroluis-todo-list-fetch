fcbd6e1c3b06a1608253566fc160e7f3d5047197
Pedro Luis Gutierrez <83560861+iampedroluis@users.noreply.github.com>
2023-08-13 00:34:08 +0000

    upload new code

--- src/js/component/Tarea.jsx
+++ src/js/component/Tarea.jsx
@@ -7,7 +7,7 @@ import React, { useState, useEffect } from 'react'
 const Tarea = () => {
 
 
-    let [listItem, setListItem] = useState([])
+    const [listItem, setListItem] = useState([])
 
 
 
@@ -24,7 +24,7 @@ const Tarea = () => {
             } else {
 
                 setListItem([...listItem, { "label": value, "done": false }])
-                agregartarea
+                
                 console.log(listItem)
                 e.target.value = ""
             }
@@ -38,54 +38,102 @@ const Tarea = () => {
 
     useEffect(() => {
         cargartareas()
-
-
-    }, [])
+    },[])
 
     useEffect(() => {
 
         agregartarea()
 
 
-    },)
+    },[listItem])
 
 
     async function cargartareas() {
-        let response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/pedro")
-        if (!response.ok) {
-            console.error("error")
-            return
-        }
-        let data = await response.json()
-        setListItem(data)
 
+        try{
+        let response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/iampedroluis")
+        if (!response.ok && response.status === 404) {
+           await  crearUsuario()  
+        }else{
+        let data = await response.json()   
+        setListItem(data)
+        
+        }
+
+
+        }catch (error){
+            console.error("Error Cargando tareas", error)
+        }
+        
+
+      
     }
 
     async function agregartarea() {
-        if (listItem.length != 0) {
+
+        if (listItem.length !== 0) {
             const requestOptions = {
                 method: 'PUT',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(listItem)
-            };
-
-            await fetch("https://playground.4geeks.com/apis/fake/todos/user/pedro", requestOptions)
+            };  
+            try {
+                await fetch("https://playground.4geeks.com/apis/fake/todos/user/iampedroluis", requestOptions)
+            } catch (error) {
+                console.error("Error agregando tarea: ", error)
+            }
+            
         }
+}
 
-
+    async function crearUsuario(){
+        const requestOptions =  {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body:JSON.stringify([])
+        }
+        try {
+            await fetch("https://playground.4geeks.com/apis/fake/todos/user/iampedroluis", requestOptions)
+        } catch (error) {
+            console.error("Error Creando usuario: ", error)
+        }
+        
+        
     }
+
+
 
     const deleteitem = (deleteitem) => {
-        console.log(deleteitem)
-        const newList = listItem.filter((el) => el !== deleteitem)
+        
+        const newList = listItem.filter((el) => el.label !== deleteitem.label)
+
         setListItem(newList);
-        console.log(listItem)
+        
+
+
+    }
+    const deleteall = () => {
+        setListItem([]);
+        
 
 
     }
 
-
-    let lista = listItem.map((el, index) => <div id="item"><li className='d-flex justify-content-between' key={index}><p className='mt-4 ms-4'>{el.label}</p> <a className='mt-3 pt-2' href='#' role='button' onClick={() => deleteitem(el)}><i id='poke' className="fa-solid fa-delete-left"></i></a></li></div>)
+    let lista = listItem.map((el, index) => (
+    <div id="item">
+        <li 
+        className='d-flex justify-content-between' 
+        key={index}>
+        <p 
+        className='mt-4 ms-4'>
+        {el.label}
+        </p> 
+        <a 
+        className='mt-3 pt-2' 
+        href='#' 
+        role='button' 
+        onClick={() => deleteitem(el)}>
+        <i id='poke' className="fa-solid fa-delete-left"></i></a></li></div>))
 
 
 
@@ -93,6 +141,10 @@ const Tarea = () => {
 
     return (
     <div className='mt-5'>
+        <div className='d-flex justify-content-end me-5'>
+        <button type="button" onClick={deleteall} class="btn btn-outline-dark deleteall">Delete All</button>
+            
+        </div>
     
             <h1 className='pt-5'>TODO LIST</h1>
             <div className='container' id='container-lista'>
@@ -107,7 +159,7 @@ const Tarea = () => {
                 </ul>
 
             </div>
-            <div className='d-flex justify-content-start text-light ps-4 pt-2' id='poke-list-foot'><p>Poke-List Items</p><p id='numberlist'>{listItem.length}</p></div>
+            <div className='d-flex justify-content-start text-light ps-4 pt-2' id='poke-list-foot'><p>List Items</p><p id='numberlist'>{listItem.length}</p></div>
             </div>
 
         </div>
